a43cef7595f5b20522c572b4fd7692f3120a6034
Ritika bindal <ritikabindal6@gmail.com>
2026-04-16 12:11:53 -0500

    Added Python letter using variables
    Created a simple Python program to generate a letter using name and address variables.

---
+++ Assiment1.py
@@ -0,0 +1,22 @@
+name = "ritika bindal"
+Age = 26 
+Address = "Delhi ,india"
+friend_name = "Neha"
+letter = f"""
+{Address}
+
+Dear {friend_name},
+
+Hi! How are you?
+
+I hope you are doing great. I just wanted to tell you that I have started learning Python programming.
+It is very interesting and I am enjoying it a lot.
+I will share more about it when we meet next time.
+Take care and stay happy!
+Your friend,  
+{name}
+"""
+
+print(letter)
+
+
