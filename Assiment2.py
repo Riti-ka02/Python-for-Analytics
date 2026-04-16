e4913aed32c22661081090171395c74a587e1b8a
Ritika bindal <ritikabindal6@gmail.com>
2026-04-16 13:08:38 -0500

    python data type

---
+++ Assiment2.py
@@ -0,0 +1,59 @@
+#1. python Data Type
+#string
+#name ="ritika bindal"
+#print("MY name is ",name)
+#print ("name data type is",type(name))''
+
+#2. numric data type;
+#rating= 200
+#print ("Rating type is",type(rating))
+
+#float
+#a = 3.40
+#print("MY data type is",type(a))
+
+#complex
+#b= 3+4J
+#print("MY data type is",type (b))
+
+#3. boolen
+#is_paid= True
+#print("My data type is",type(is_paid))
+
+#4.sequence
+#cities=["delhi","mumbai","chennai"]
+#print(cities)
+#print(type(cities))
+
+#dd=(1990,1991)
+#print(dd)
+#print(type(dd))
+
+#d={"cat","dog","tiger"}
+#print(d)
+#print(type(d))
+ 
+#5.range
+#num =range(1,11) 
+#print(list(num)) #[0,1,2,3,4]
+#print (type(num))
+ 
+#days=range(1,31)
+#print(list(days))
+
+#6.dict
+#student = {
+  #  "name":"Ritika binda",
+#    "age":25,
+#    "city":"delhi"
+#print(student)
+#print(type(student))
+
+#7.set
+#number ={1,2,2,3,4}
+#print(number)
+
+#8. none datatype
+remark=None
+print(remark,type(remark))
+ 
