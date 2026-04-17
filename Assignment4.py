8f817ef49e096608b5d159be855dcde1667ac04f
Ritika bindal <ritikabindal6@gmail.com>
2026-04-17 14:28:45 -0500

    Python inputs and typecasting

---
+++ Assignment4.py
@@ -0,0 +1,25 @@
+ # Input and Typecsting
+#name = input("enter your name")
+#print("welcome",name)
+
+#AGE =int(input("enter the age "))
+#AGE +=5
+#print("my age is",AGE)
+
+#temprature=float(input("enter the temprature"))
+#print("today temprature is",temprature)
+#print(type(temprature))
+
+#convert number to string
+#sales=5000
+#text="total sale:" + str(sales)
+#print(text)
+
+#total sale calculation
+name=input("enter your Product name:")
+product_Quantity=int(input("enter the product quantity:"))
+Price_of_Product=float(input("enter the price of product:"))
+
+total = product_Quantity*Price_of_Product
+print("purchaser name is",name)
+print("total sale is ",total)
