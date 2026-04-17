58cf175ac5e9a08fce05a373cd525d34ed7d128f
Ritika bindal <ritikabindal6@gmail.com>
2026-04-17 13:21:51 -0500

    Operators in python
    in this  assignment , we use different different type of operators , like, Arithmatic, comparison,logical,and etc...

---
+++ Assiment3.py
@@ -0,0 +1,37 @@
+#Arithmetic opreator
+#a=20
+#b=30
+#print(a+b) # addition 
+#print(a-b) #subtrication
+
+#Assingment operator
+#value=50
+#print(value)
+
+#value = - 10
+#print(value) #subtract and assin
+
+#comparison operator
+#p = 12
+#p1=35
+#print(p<p1)
+#print(p>p1)
+
+#Logical operator
+#x=2
+#x1=3
+#x2=4
+#x3=5
+#print(x>x1 and x2>x3) #and condition 
+#print(x>x1 or x2>x3) # or condition
+
+#identity operator
+#m1=100
+#m2=100
+#print(m1 is m2)
+
+#membership operator
+print("check 'a' in 'mango' :", 'a' in 'mango')
+
+
+
